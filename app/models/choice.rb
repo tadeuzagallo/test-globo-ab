4d42680e815c447106937284a025033d87fb8b25
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-04-18 16:42:51 -0300

    validate numericality of weight

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -3,5 +3,6 @@ require 'active_record'
 class Choice < ActiveRecord::Base
   belongs_to :test
 
-  validates :url, :weight , presence: true
+  validates :url, presence: true
+  validates :weight, presence: true, numericality: true
 end
--- spec/models/choices_spec.rb
+++ spec/models/choices_spec.rb
@@ -3,4 +3,5 @@ require 'choice'
 describe(Choice) do
   it { should validate_presence_of :url }
   it { should validate_presence_of :weight }
+  it { should validate_numericality_of :weight }
 end
