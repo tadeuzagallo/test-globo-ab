d23781b8168c440ac48aa3d2d462c053b332a6c9
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-04-18 16:23:42 -0300

    add dependent destroy to test's choices

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ require 'active_record'
 require 'choice'
 
 class Test < ActiveRecord::Base
-  has_many :choices
+  has_many :choices, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :choices, presence: true
