57dc8a68d30d66f9153e1ddf92433f3a567491df
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-04-18 17:35:42 -0300

    validate user_choice user_id

--- app/models/user_choice.rb
+++ app/models/user_choice.rb
@@ -1,3 +1,5 @@
 class UserChoice < ActiveRecord::Base
   self.table_name = :users_choices
+
+  validates :user_id, presence: true, numericality: true
 end
--- spec/models/users_choices_spec.rb
+++ spec/models/users_choices_spec.rb
@@ -1,4 +1,6 @@
 require 'user_choice'
 
 describe(UserChoice) do
+  it { should validate_presence_of :user_id }
+  it { should validate_numericality_of :user_id }
 end
