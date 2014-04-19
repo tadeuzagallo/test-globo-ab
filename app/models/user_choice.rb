ae1bb879f7b6f7f22001f44cd36316f7a77d219b
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-04-19 12:49:46 -0300

    validate uniquiness of choice for each test for each user

--- app/models/user_choice.rb
+++ app/models/user_choice.rb
@@ -1,5 +1,21 @@
 class UserChoice < ActiveRecord::Base
   self.table_name = :users_choices
 
+  before_validation :set_test_id
+
+  belongs_to :choice
+
   validates :user_id, presence: true, numericality: true
+  validates :choice, presence: true 
+  validates :test_id, uniqueness: { scope: :user_id }
+
+  private
+
+  def set_test_id
+    if self.choice.present?
+      self.test_id = self.choice.test_id
+    end
+
+    true
+  end
 end
--- spec/models/users_choices_spec.rb
+++ spec/models/users_choices_spec.rb
@@ -3,4 +3,17 @@ require 'user_choice'
 describe(UserChoice) do
   it { should validate_presence_of :user_id }
   it { should validate_numericality_of :user_id }
+  it { should validate_presence_of :choice }
+
+  let (:test) { Test.crete(name: 'A/B test', choices: [choice1, choice2]) }
+  let(:choice1) { Choice.create(url: 'http://google.com', weight: 2)}
+  let(:choice2) { Choice.create(url: 'http://yahoo.com', weight: 1)}
+
+  it 'should validate uniqueness of choice for a given user' do
+    choice = UserChoice.create({ user_id: 1, choice: choice1 })
+    choice.should be_valid
+
+    choice = UserChoice.create({ user_id: 1, choice: choice2 })
+    choice.should_not be_valid
+  end
 end
