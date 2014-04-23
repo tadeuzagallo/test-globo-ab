ca732117a7d4b42a0f91b4b564b89da902e20c86
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-04-23 14:31:19 -0300

    destroy users_choices when choice is destroyed

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,7 +2,7 @@ require 'active_record'
 
 class Choice < ActiveRecord::Base
   belongs_to :test
-  has_many :users_choices, class_name: 'UserChoice'
+  has_many :users_choices, class_name: 'UserChoice', dependent: :destroy
 
   validates :url, presence: true, format: URI.regexp
   validates :weight, presence: true, numericality: true
